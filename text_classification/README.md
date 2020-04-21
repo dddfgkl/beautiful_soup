e5bf8068db2eecb55fd007a1429a8f79736a56a3
macong <macong.ucaser@gmail.com>
2020-04-22 00:38:14 +0800

    night good

---
+++ text_classification/README.md
@@ -0,0 +1,20 @@
+# 文本分类  
+如题  
+
+### 1、数据格式  
+- 数据描述文件: meta.json      
+- 训练集: train.tsv       
+- 测试集: valid.tsv       
+
+
+### 2、model  
+
+
+
+
+# info  
+author:macong.ucaser@gmail.com    
+FirstEdit: 2020.04.22 00:25     
+LastEdit:    
+
+
--- text_classification/make_meta.py
+++ text_classification/make_meta.py
@@ -3,3 +3,31 @@ import json
 import numpy as np
 import pandas as pd
 
+"""
+    Author: macoong.ucaser@gmail.com  
+    FistEdit: 2020.04.22 00:35  
+    LastEdit:  
+"""
+
+def construct_meta(train_csv, valid_csv, meta_pos):
+
+    df_train = pd.read_csv(train_csv, delimiter=",", encoding="utf-8")
+    df_test = pd.read_csv(valid_csv, delimiter=",", encoding="utf-8")
+    class_num = 2
+
+    meta = {}
+    meta["task"] = "classification"
+    # meta["language"] = "ZH"
+    meta["language"] = "EN"
+    meta["train_num"] = len(df_train)
+    meta["valid_num"] = len(df_test)
+    meta["class_num"] = class_num
+
+    print()
+    print(meta)
+
+    # write meta
+    with open(os.path.join(meta_pos, "meta.json"), 'w') as outfile:
+        outfile.write('{}\n'.format(json.dumps(meta, indent=4)))
+
+    print("construct meta over")

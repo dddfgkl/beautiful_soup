19cb4c11de826fadb8f308a2307acdb6cd6f3485
macong <macong.ucaser@gmail.com>
2020-04-28 09:51:58 +0800

    add

---
+++ text_classification/src/textcnn/textcnn2.py
@@ -0,0 +1,86 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+dtype = torch.FloatTensor
+
+# Text-CNN Parameter
+embedding_size = 2 # n-gram
+sequence_length = 3
+num_classes = 2  # 0 or 1
+filter_sizes = [2, 2, 2] # n-gram window
+num_filters = 3
+
+# 3 words sentences (=sequence_length is 3)
+sentences = ["i love you", "he loves me", "she likes baseball", "i hate you", "sorry for that", "this is awful"]
+labels = [1, 1, 1, 0, 0, 0]  # 1 is good, 0 is not good.
+
+word_list = " ".join(sentences).split()
+word_list = list(set(word_list))
+word_dict = {w: i for i, w in enumerate(word_list)}
+vocab_size = len(word_dict)
+
+inputs = []
+for sen in sentences:
+    inputs.append(np.asarray([word_dict[n] for n in sen.split()]))
+
+targets = []
+for out in labels:
+    targets.append(out) # To using Torch Softmax Loss function
+
+input_batch = Variable(torch.LongTensor(inputs))
+target_batch = Variable(torch.LongTensor(targets))
+
+
+class TextCNN(nn.Module):
+    def __init__(self):
+        super(TextCNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embedding_size)
+        self.conv1ds = nn.ModuleList(
+            [nn.Sequential(
+                nn.Conv1d(embedding_size, num_filters, kernel_size=filter_sizes[index]),
+                nn.ReLU()) for index in range(len(filter_sizes))]
+        )
+        self.fc1 = nn.Linear(len(filter_sizes)*num_filters, num_classes)
+
+    def forward(self, x):
+        out = self.embed(x)
+        out = torch.transpose(out, 1, 2)
+        out = [conv(out) for conv in self.conv1ds]
+        out = [F.max_pool1d(c_out, kernel_size= c_out.size(2)).seqeeze(2) for c_out in out]
+        out = torch.cat(out, 1)
+        logit = self.fc1(out)
+        return logit
+
+model = TextCNN()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training
+for epoch in range(5000):
+    optimizer.zero_grad()
+    output = model(input_batch)
+
+    # output : [batch_size, num_classes], target_batch : [batch_size] (LongTensor, not one-hot)
+    loss = criterion(output, target_batch)
+    if (epoch + 1) % 1000 == 0:
+        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+
+    loss.backward()
+    optimizer.step()
+
+# Test
+test_text = 'sorry hate you'
+tests = [np.asarray([word_dict[n] for n in test_text.split()])]
+test_batch = Variable(torch.LongTensor(tests))
+
+# Predict
+predict = model(test_batch).data.max(1, keepdim=True)[1]
+if predict[0][0] == 0:
+    print(test_text,"is Bad Mean...")
+else:
+    print(test_text,"is Good Mean!!")

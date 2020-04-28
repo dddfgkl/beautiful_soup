3937e5dc9ed7399259c438844978d5560375d5a8
macong <macong.ucaser@gmail.com>
2020-04-28 12:03:25 +0800

    add

--- text_classification/src/textcnn/textcnn2.py
+++ text_classification/src/textcnn/textcnn2.py
@@ -61,7 +61,7 @@ for epoch in range(100):
     # output : [batch_size, num_classes], target_batch : [batch_size] (LongTensor, not one-hot)
     loss = criterion(output, target_batch)
 
-    corrects = (torch.max(output, 1)[1].view(targets.size()).data == target_batch.data).sum()
+    corrects = (torch.max(output, 1)[1].view(target_batch.size()).data == target_batch.data).sum()
     accuracy = corrects / len(target_batch.data)
 
     if (epoch + 1) % 2 == 0:
